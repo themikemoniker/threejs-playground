f8c53840a3916442b5e85eb10dc8a969a9e80af1
Mike Moniker <themikemoniker@proton.me>
2024-12-20 18:05:39 -0600

    2 added geometry to scene

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshBasicMaterial({
+    color: 0xccff
+})
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
 renderer.render(scene, camera);
