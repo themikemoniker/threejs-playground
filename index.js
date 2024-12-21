d5556bc32fc1c5e15aea77d6a76fa82f6bd494bc
Mike Moniker <themikemoniker@proton.me>
2024-12-20 18:17:31 -0600

    4 add y rotation and wire mesh

--- index.js
+++ index.js
@@ -15,15 +15,27 @@ camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshBasicMaterial({
-    color: 0xccff
-})
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading: true
+});
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001)
+mesh.add(wireMesh)
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000)
+scene.add(hemiLight);
+
 function animate(t = 0){
     requestAnimationFrame(animate);
-    mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0);
+    mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
 }
 animate();
