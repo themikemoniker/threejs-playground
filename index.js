17340b3e9822e29f79bb0f774cbb5bcebc8c21a7
Mike Moniker <themikemoniker@proton.me>
2024-12-20 18:22:21 -0600

    6 added controls/damping

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js"
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -13,6 +14,10 @@ const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
 
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial({
@@ -37,5 +42,6 @@ function animate(t = 0){
     requestAnimationFrame(animate);
     mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
+    controls.update();
 }
 animate();
